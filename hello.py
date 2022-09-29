68722cb04e7f950972bf76dca3de49843439e8ce
Shashwat Panwar <shashwatpanwar17@gmail.com>
2022-09-28 22:34:02 -0400

    added email checking and email change detection and uoft email verification

--- hello.py
+++ hello.py
@@ -5,7 +5,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
+# import email
 
 
 app = Flask(__name__)
@@ -16,23 +17,37 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = StringField('What is your UofT Email Address?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[Email()])
     submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index(): 
     # return render_template('index.html', current_time = datetime.utcnow())
     name = None
     form = NameForm()
+
+    # email_form = NameEmailForm()
+
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+        
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
 
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your name!')
+        session['email'] = form.email.data
+
+        if 'utoronto' in session['email']:
+            session['uoft'] = True
+        else:
+            session['uoft'] = False
+            
         return redirect(url_for('index'))
 
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), uoft = session.get('uoft'))
 
 @app.route('/user/<name>')
 def user(name):
