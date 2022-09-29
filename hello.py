ac8942d4b1e8afd91539de0baf9a1b23e031dbc2
Shashwat Panwar <shashwatpanwar17@gmail.com>
2022-09-28 20:08:30 -0400

    added base.html, added current time feature to user and index

--- hello.py
+++ hello.py
@@ -1,16 +1,24 @@
+from ensurepip import bootstrap
 from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index(): 
-    return render_template('index.html')
+    return render_template('index.html', current_time = datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time = datetime.utcnow())
 
     
 if __name__ == '__main__':
