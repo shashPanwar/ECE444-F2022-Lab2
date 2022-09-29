a5ff887f8e5249c8fe15b9a0755710a954d5a770
Shashwat Panwar <shashwatpanwar17@gmail.com>
2022-09-28 20:12:41 -0400

    reinstalled moment and bootstrap

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
 from ensurepip import bootstrap
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+
 
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
